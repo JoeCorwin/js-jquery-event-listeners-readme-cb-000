b00b599961236a9051c70188e70ac676b38468a9
Joe Corwin <joe.corwin@gmail.com>
2019-01-31 02:23:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,13 @@
-function getIt() {
-  $('h1').on("click", function(){
-  alert ("Hey!"))
-  }
-};
+
 
   //define functions here
 
 $(document).ready(function(){
 getIt()
-// call functions here
+function getIt() {
+  $('h1').on("click", function(){
+  alert ("Hey!"))
+  }
+};
 
 });
