6feac0502203a202e927ea1f21c24165946dea38
Joe Corwin <joe.corwin@gmail.com>
2019-01-31 02:26:23 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,13 @@
-
-
-  //define functions here
-
-$(document).ready(function(){
-
 function getIt() {
   $(document).on("click", function(){
   alert ("Hey!"))
   }
 };
+
+  //define functions here
+
+$(document).ready(function(){
+  getIt()
+
 
 });
